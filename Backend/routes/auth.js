6134fe67c63e327bcb901ac4d038bb9b2fe5d0d0
Ankush <ankushyd14@gmail.com>
2023-03-20 12:31:16 +0530

    working on backend validation

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,15 +6,23 @@ const { body, validationResult } = require('express-validator');
 // Creating  a user Using: POST "/api/create user". No login required
 
 router.post('/createuser',[
-   body ('name','Enter valid name').isLength({min:5}),
-    body('email').isEmail(),
+   body ('name','Enter valid name').isLength({min:3}),
+    body('email','Enter valid email').isEmail(),
+    body ('password','password must be atleast 8 characters').isLength({min:8}),
 ],async(req,res)=>{
 console.log(req.body);
 const errors = validationResult(req);
 if (!errors.isEmpty()) {
   return res.status(400).json({ errors: errors.array() });
 }
+let user =await User.create ({
+  name: req.body.name,
+  password: req.body.password,
+  email: req.body.email,
+}).then(user => res.json(user))
+.catch(err=>{console.log(err)
+res.json({error:'please enter a unique email', message: err.message})})
 
-res.send(req.body);
+
 })
 module.exports = router
