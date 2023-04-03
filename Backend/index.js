af926f5b25948a8079a27876f9c0221864b6e75d
Ankush <ankushyd14@gmail.com>
2023-04-03 11:07:23 +0530

    minor changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 const connectToMongo = require('./DB');
 const express = require('express')
 
+/* Connecting to the MongoDB database. */
 connectToMongo();
 
+/* *|MARKER_CURSOR|* */
 const app = express()
 const port = 5000
 app.use(express.json())
@@ -16,6 +18,7 @@ app.get('/', (req, res) => {
   res.send('ksakai Mandali!')
 })
 
+/* Listening to the port 5000. */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
