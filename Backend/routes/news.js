8d3a9342f4ee8c49ef46e6b213e0a312ff18af6a
Ankush <ankushyd14@gmail.com>
2023-04-09 11:06:31 +0530

    adding comments

--- Backend/routes/news.js
+++ Backend/routes/news.js
@@ -33,6 +33,11 @@ router.post("/addNews", fetchuser, async (req, res) => {
       });
       
   
+  /* This code block is handling a POST request to add news to the database. It first extracts the
+  necessary data from the request body and creates a new News object with that data. It then saves
+  the News object to the database using the `save()` method and sends a JSON response with the saved
+  News object and a status code of 200. If there is an error during this process, it logs the error
+  message and sends a response with a status code of 500. */
       const savedNews = await news.save();
       res.status(200).json(savedNews);
     } catch (error) {
@@ -40,6 +45,12 @@ router.post("/addNews", fetchuser, async (req, res) => {
       res.status(500).send("Internal Server Error");
     }
   });
+ /* This code block is defining a route for retrieving all news articles from the database using the
+ HTTP GET method. It first uses the `News.find()` method to retrieve all news articles from the
+ database and stores them in the `news` variable. It then checks the length of the `news` array and
+ if it is zero, it sends a JSON response with an error message and a status code of 404. If there
+ are news articles in the database, it sends a JSON response with the news articles and the total
+ number of news articles in the database with a status code of 200. */
   router.get("/allNews",  async (req, res) => {
     try {
       const news = await News.find();
